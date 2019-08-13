dd3136c017b93cb631420c860df63292e1dbb532
sembenavente <sembenavente@upeu.edu.pe>
2019-08-13 18:23:10 -0500

    second commit

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,15 @@
+import { Cliente } from "./cliente";
+
+export const CLIENTES: Cliente[]= [
+    {id: 1, nombre: 'Andrés', apellido:'Guzmán', email:'profesor@bolsadeideas.com', createAt:'2017-12-11'},
+    {id: 2, nombre: 'Valeria', apellido:'bracho', email:'Valeria@bolsadeideas.com', createAt:'2017-12-11'},
+    {id: 3, nombre: 'Jennifer', apellido:'Barrera', email:'Jennifer@bolsadeideas.com', createAt:'2017-12-11'},
+    {id: 4, nombre: 'Angelika', apellido:'Gonzales', email:'Angelika@bolsadeideas.com', createAt:'2017-12-11'},
+    {id: 5, nombre: 'Danfer', apellido:'Vergara', email:'prVergaraofesor@bolsadeideas.com', createAt:'2017-12-11'},
+    {id: 6, nombre: 'Yeiser', apellido:'Navarro', email:'Navarro@bolsadeideas.com', createAt:'2017-12-11'},
+    {id: 7, nombre: 'Rubi', apellido:'Tito', email:'Rubi@bolsadeideas.com', createAt:'2017-12-11'},
+    {id: 8, nombre: 'Elizabeth', apellido:'Benavente', email:'Elizabeth@bolsadeideas.com', createAt:'2017-12-11'},
+    {id: 9, nombre: 'Josue', apellido:'Malca', email:'Josue@bolsadeideas.com', createAt:'2017-12-11'},
+    {id: 10, nombre: 'Mijares', apellido:'Parada', email:'Parada@bolsadeideas.com', createAt:'2017-12-11'},
+    {id: 11, nombre: 'Samuel', apellido:'Colina', email:'Colina@bolsadeideas.com', createAt:'2017-12-11'}
+  ];
